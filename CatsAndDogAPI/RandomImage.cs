87dac06cdbbe1a39bfcf701aa5175e24cb1bd9c4
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-11-23 16:39:24 -0800

    Add project files.

---
+++ CatsAndDogAPI/RandomImage.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+
+namespace CatsAndDogAPI
+{
+    /// <summary>
+    /// A random image object from the API
+    /// </summary>
+    public class RandomImage
+    {
+        public string id { get; set; } = null!;
+        public string url { get; set; } = null!;
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+
+    public class CatAndDogAPIHelper
+    {
+        private static readonly HttpClient _httpClient = new();
+
+        static CatAndDogAPIHelper()
+        {
+            _httpClient.BaseAddress = new Uri("https://api.thecatapi.com/v1/");
+        }
+
+        /// <summary>
+        /// Get a specific number of random images
+        /// form the cat and dog API
+        /// </summary>
+        /// <param name="numImages"> Number of images to retrieve </param>
+        /// <returns></returns>
+        public static async Task<RandomImage[]?> GetRandomImages(int numImages = 1)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("images/search");
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                RandomImage[]? randomImages = JsonConvert.DeserializeObject<RandomImage[]>(data);
+                return randomImages;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ CatsAndDogWeb/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using CatsAndDogAPI;
+using CatsAndDogWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatsAndDogWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            RandomImage[]? response = await CatAndDogAPIHelper.GetRandomImages();
+            return View(response);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
